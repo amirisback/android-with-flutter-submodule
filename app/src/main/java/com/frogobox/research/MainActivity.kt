621c167dfd2f40c66c1842736f64c3ae873a4c8b
amirisback <faisalamircs@gmail.com>
2022-07-15 12:45:07 +0700

    add: call flutter project app

--- app/src/main/java/com/frogobox/research/MainActivity.kt
+++ app/src/main/java/com/frogobox/research/MainActivity.kt
@@ -2,10 +2,13 @@ package com.frogobox.research
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startActivity(FlutterActivity.createDefaultIntent(this))
     }
 }
